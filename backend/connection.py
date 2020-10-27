d0193e2f0848fddecd04a766bae3291433dc8007
Sungjin Kim <68371801+kimsj5259@users.noreply.github.com>
2020-10-27 14:34:09 +0900

    Update connection.py

--- backend/connection.py
+++ backend/connection.py
@@ -1,8 +1,21 @@
-import pymysql.cursors
+import pymysql
 
 from config  import database 
 
 def get_connection():
+    """
+    import 되서 사용될 때마다 connection 생성
+    
+    returns :
+        database connection 객체
+    
+    Authors :
+        jisunn0130@gmail.com (최지선) , kimsj5259@gmail.com (김성진)
+    
+    History :
+        2020-10-27  (jisunn0130@gmail.com) : 초기생성
+    """
+    
     connection = pymysql.connect(
             host = database['host'],
             port = database['port'],
@@ -11,5 +24,6 @@ def get_connection():
             database = database['database'],
             charset  = database['charset'],
             cursorclass = pymysql.cursors.DictCursor)
+    
     return connection
 
