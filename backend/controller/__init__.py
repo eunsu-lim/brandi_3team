2ffa7f612343b915a83a66a214241b6729bf19fb
jisun <jisunn0130@gmail.com>
2020-11-10 09:41:33 +0900

    seller(home)차트 API & 주문리스트 보기, 구매하기, 주문상태 변경 API

--- backend/controller/__init__.py
+++ backend/controller/__init__.py
@@ -1,7 +1,10 @@
 from .account_controller import create_account_endpoints
 from .seller_controller  import create_seller_endpoints
+from .order_controller   import create_order_endpoints
+
 
 __all__ = [
     'create_account_endpoints',
-    'create_seller_endpoints'
+    'create_seller_endpoints',
+    'create_order_endpoints'
 ]
